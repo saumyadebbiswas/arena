c9c315dbfc15f58a98753d75def60cb6f1ced6d2
saumyadebbiswas <>
2019-12-05 12:51:09 +0530

    Global api link

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,2 +1,2 @@
-export let SITE_URL = "http://localhost/arena"; //--- This is the site url use everywhere
-export let API_LINK = "http://localhost/arena/api"; //--- This is the API link use everywhere
+export let SITE_URL = "http://webideasole.com/arena"; //--- This is the site url use everywhere
+export let API_LINK = "http://webideasole.com/arena/api"; //--- This is the API link use everywhere
